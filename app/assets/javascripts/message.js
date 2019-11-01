878a1b353e99197230a25060bbd12b3f6255232e
kenken0327 <56866634+kenken0327@users.noreply.github.com>
2019-11-01 21:21:38 +0900

    連続でSENDボタンを押せるようにした
    message.jsの.doneに下記の追加
    $(".submit-btn").prop('disabled', false);
          $('#message_content').val("")

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,6 +39,8 @@ $(function(){
       $('.messages').animate({ 
         scrollTop: $('.messages')[0].scrollHeight
       }, 'fast');
+      $(".submit-btn").prop('disabled', false);
+      $('#message_content').val("")
     })
     .fail(function(){
     })
