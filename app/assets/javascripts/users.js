ae515cf1eb2affef97a0a4e4eb69a18eb7c3a62a
kenken0327 <56866634+kenken0327@users.noreply.github.com>
2019-11-04 13:36:56 +0900

    非同期通信の結果をdone関数で受取り、ビューに追加するためのHTMLを作成

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,18 +1,38 @@
 $(function(){
-  $("#user-search-field").on( "keyup", function(){
-    let input = $("#user-search-field").val();
+  function searchUser(user){
 
-    $.ajax({
-      type: "GET",
-      url: "/users",
-      dataType: "json",
-      data: {keyword: input}
-    })
-      .done(function(users){
-        console.log("成功です");
+    var html = `<div class="chat-group-user clearfix">
+          <p class="chat-group-user__name">${user.name}</p>
+            <a class="user-search-add
+             chat-group-user__btn
+             chat-group-user__btn--add"
+             data-user-id= ${user.id}
+             data-user-name=${user.name}>追加</a>
+        </div>`
+    return html;
+  }
+
+  $("#user-search-field").on("keyup", function() {
+      var input = $("#user-search-field").val();
+
+      $.ajax({
+        url: '/users',
+        type: "GET",
+        data: { keyword: input },
+        dataType: 'json'
       })
-      .fail(function(){
-        console.log("失敗です");
+    .done(function(users) {
+      $('#user-search-result').empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          searchUser(user);
+          var html = searchUser(user);
+          $('#user-search-result').append(html)
+        });
+      };
       })
+      .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    });
   });
 });
