580ea62d1c9c1202f2f7ceccf91f60be79216430
kenken0327 <56866634+kenken0327@users.noreply.github.com>
2019-11-07 20:29:41 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,7 @@
 
-
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock "~> 3.11.2"
+lock "3.11.2"
 
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
