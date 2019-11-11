9e2fc4c50f730a813d1a83b11590ec70d31445a5
kenken0327 <56866634+kenken0327@users.noreply.github.com>
2019-11-11 11:18:48 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -38,7 +38,7 @@ $(document).on('turbolinks:load', function(){
     .done(function(data){
       var html = buildMessage(data);
       $('.messages').append(html);
-      $('#form')[0].reset();
+      $(".form")[0].reset();
       $('.messages').animate({ 
         scrollTop: $('.messages')[0].scrollHeight
       }, 'fast');
